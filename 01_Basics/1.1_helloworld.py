67dd275b6c420e80a31f233e200f589cc6c06185
yashrajsen <yashrajsen@gmail.com>
2022-06-09 03:07:32 +0530

    master

--- 01_Basics/1.1_helloworld.py
+++ 01_Basics/1.1_helloworld.py
@@ -1,7 +1,18 @@
 # Example of Hello World
 
-def main():
-     print("Hello World 2.0")
+# def main():
+#      print("Hello World 2.0")
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+# t = (1,2,4,3)
+# # t[3]=45
+# print(len(t))
+
+# var = 'hello python'
+# newvar = list(var.upper())
+# print(newvar)
+
+s = "hello"
+print(type(s))
